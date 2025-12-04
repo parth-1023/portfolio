e7b1c27a00a29fcff7bbc434d6f7b84d1330eba3
Parth Shroff <pshroff@wpi.edu>
2025-12-04 01:33:40 -0500

    Togglebutton error

--- src/components/sidebar/toggleButton/ToggleButton.jsx
+++ src/components/sidebar/toggleButton/ToggleButton.jsx
@@ -1,6 +1,6 @@
 import { motion } from "framer-motion";
 
-const ToggleButton = ({ setOpen, state = "closed" }) => {
+const Togglebutton = ({ setOpen, state = "closed" }) => {
   return (
     <button onClick={() => setOpen((prev) => !prev)}>
       <svg width="23" height="23" viewBox="0 0 23 23">
@@ -38,4 +38,4 @@ const ToggleButton = ({ setOpen, state = "closed" }) => {
   );
 };
 
-export default ToggleButton;
+export default Togglebutton;
